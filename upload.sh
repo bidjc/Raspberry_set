95bac94c91748919372c3235d3c825df0bddbff8
bidjc <563200085@qq.com>
2016-04-04 14:29:49 +0800

    更新git操作脚本

--- upload.sh
+++ upload.sh
@@ -6,53 +6,87 @@
 #########################################################################
 #!/bin/bash
 Nmb=$#
+a=($@)
+nnum=0;
+
+CheckVarNum(){
+	if [ $1 == "0" ] ;then
+	    echo -e "eg: upload.sh add filename\nupload.sh init"
+   	 	exit
+	else
+    	echo "do $1"
+	fi
+}
+CheckCmd(){
+		#echo "aa = ${a[$num]}"
+	var=${a[$nnum]}
+	echo "Check cmd:$var"
+	echo "current var: $var"
+	if [ $var == "test" ];then
+    	echo "ssh -T git@github.com"
+    	ssh -T git@github.com   #测试是否连接github成功
+		nnum=$[$nnum+1]
+	elif [ $var == "init" ];then
+    	echo "git init"
+	    git init  #此时git会在这个文件夹下创建一个隐藏目录，这个目录就是本地库了
+		nnum=$[$nnum+1]
+	elif [ $var == "bound" ];then
+    	if [ ! -n "${a[$nnum+1]}" -o ! -n "${a[$nnum+2]}" ];then
+        	echo "eg: upload.sh bound bidjc Raspberry"
+	        exit
+	    fi  
+		git remote add origin git@github.com:"$2"/"$3".git
+		nnum=$[$nnum+3]
+	elif [ $var == "down" ];then
+    	git pull origin master
+		nnum=$[$nnum+1]
+	elif [ $var == "add" ];then
+		value1=${a[$nnum+1]}
+		value2=${a[$nnum+2]}
+    	if [ ! -n "$value1" -o ! -n "$value2" ];then
+        	echo "eg: upload.sh add filename file discribe"
+	        exit
+    	fi  
+	    git add $value1
+    	git commit -m "$value2"
+	    git push origin master
+		nnum=$[$nnum+3]
+	elif [ $var == "addall" ];then
+		value1=${a[$nnum+1]}
+    	if [ ! -n "$value1" ];then
+	        echo "eg: upload.sh addall discribe"
+    	    exit
+	    fi  
+    	git add .
+	    git commit -m "$value1"
+    	git push origin master
+		nnum=$[$nnum+2]
+	else
+		nnum=$[$nnum+1]
+fi
+}
 PrintVar(){
-	aa=$1
-	for i in $1[@];do
-		echo $i
+	num=1
+	#local aa=($1)
+	#for i in ${aa[*]};do
+#	for i in ${a[*]};do
+#		echo "var $num: $i"
+#		CheckCmd
+#		
+#		let num+=1
+#	done
+	while [[ $nnum < $Nmb ]]
+	do
+		a1=${a[$nnum]}
+		echo "var $num: $a1"
+		CheckCmd
+		let num+=1
+	#	echo "nnum=[$nnum]	$Nmb=[$Nmb]"
 	done
 }
-a=($@)
-PrintVar $a
-echo "number is $Nmb"
-echo $1
-if [ ! -n "$1" ] ;then
-	echo "eg: upload.sh add filename\nupload.sh init"
-	exit
-else
-	echo "do $1"
-fi
-if [ $1 == "test" ];then
-	echo "ssh -T git@github.com"
-	ssh -T git@github.com   #测试是否连接github成功
-elif [ $1 == "init" ];then
-	echo "git init"
-	git init  #此时git会在这个文件夹下创建一个隐藏目录，这个目录就是本地库了
-elif [ $1 == "bound" ];then
-	if [ ! -n "$2" -o ! -n "$3" ];then
-		echo "eg: upload.sh bound bidjc Raspberry"
-		exit
-	fi
-	git remote add origin git@github.com:"$2"/"$3".git
-elif [ $1 == "down" ];then
-	git pull origin master
-elif [ $1 == "add" ];then
-	if [ ! -n "$2" -o ! -n "$3" ];then
-        echo "eg: upload.sh add filename file discribe"
-        exit
-    fi	
-	git add $2
-	git commit -m "$3"
-	git push origin master
-elif [ $1 == "addall" ];then
-    if [ ! -n "$2" ];then
-        echo "eg: upload.sh addall discribe"
-        exit
-    fi  
-    git add .
-    git commit -m "$2"
-    git push origin master
-fi
+CheckVarNum $Nmb
+
+PrintVar "${a[*]}"
 
 #git remote add origin git@github.com:bidjc/Raspberry.git
 #git pull origin master
