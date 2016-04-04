64b35821af51b89f6b8c5279afc14f7d93664ada
bidjc <563200085@qq.com>
2016-04-04 11:53:23 +0800

    git操作脚本

---
+++ upload.sh
@@ -0,0 +1,40 @@
+#########################################################################
+# File Name: upload1.sh
+# Author: dengguangjie
+# mail: 563200085@qq.com
+# Created Time: 2016年04月04日 星期一 11时14分47秒
+#########################################################################
+#!/bin/bash
+echo $1
+if [ ! -n "$1" ] ;then
+	echo "eg: upload.sh add filename\nupload.sh init"
+	exit
+else
+	echo "do $1"
+fi
+if [ $1 == "test" ];then
+	echo "ssh -T git@github.com"
+	ssh -T git@github.com   #测试是否连接github成功
+elif [ $1 == "init" ];then
+	echo "git init"
+	git init  #此时git会在这个文件夹下创建一个隐藏目录，这个目录就是本地库了
+elif [ $1 == "bound" ];then
+	if [ ! -n "$2" -o ! -n "$3" ];then
+		echo "eg: upload.sh bound bidjc Raspberry"
+		exit
+	fi
+	git remote add origin git@github.com:"$2"/"$3".git
+elif [ $1 == "down" ];then
+	git pull origin master
+elif [ $1 == "add" ];then
+	if [ ! -n "$2" -o ! -n "$3" ];then
+        echo "eg: upload.sh add filename file discribe"
+        exit
+    fi	
+	git add $2
+	git commit -m "$3"
+	git push origin master
+fi
+
+#git remote add origin git@github.com:bidjc/Raspberry.git
+#git pull origin master
